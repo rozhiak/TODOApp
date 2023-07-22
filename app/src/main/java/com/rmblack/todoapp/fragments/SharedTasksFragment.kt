c547187479f6a748fc791b2ccefe1ae7f073d6f9
rozhiak <fhvs1234t>
2023-07-22 19:46:45 +0330

    Set up fragments

---
+++ app/src/main/java/com/rmblack/todoapp/fragments/SharedTasksFragment.kt
@@ -0,0 +1,34 @@
+package com.rmblack.todoapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rmblack.todoapp.databinding.FragmentSharedTasksBinding
+
+class SharedTasksFragment : Fragment() {
+
+    private lateinit var binding: FragmentSharedTasksBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentSharedTasksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            //Here wire up views.
+        }
+    }
+
+}
