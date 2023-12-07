67f45c8ad5ca3e8b366eb76676a75bdeb7c93e44
rozhiak <fhvs1234t>
2023-12-07 17:50:08 +0330

    cleaning - position of error tv in verification fragment

--- app/src/main/java/com/rmblack/todoapp/fragments/PrivateTasksFragment.kt
+++ app/src/main/java/com/rmblack/todoapp/fragments/PrivateTasksFragment.kt
@@ -6,14 +6,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.marginTop
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.rmblack.todoapp.adapters.PrivateTasksAdapter
 import com.rmblack.todoapp.viewmodels.PrivateTasksViewModel
 import kotlinx.coroutines.launch
@@ -76,16 +74,17 @@ class PrivateTasksFragment : TasksFragment() {
                     editedTaskId = null
                     isNewTask = null
 
-                    val layoutManager = binding.tasksRv.layoutManager as LinearLayoutManager
-                    val firstVisibleItem = layoutManager.getChildAt(0)
-                    val offset = firstVisibleItem?.top ?: 0
-                    val pos = layoutManager.findFirstVisibleItemPosition()
-                    val marginTop = firstVisibleItem?.marginTop ?: 0
+                    // TODO: if there was no problem , delete commented code
+//                    val layoutManager = binding.tasksRv.layoutManager as LinearLayoutManager
+//                    val firstVisibleItem = layoutManager.getChildAt(0)
+//                    val offset = firstVisibleItem?.top ?: 0
+//                    val pos = layoutManager.findFirstVisibleItemPosition()
+//                    val marginTop = firstVisibleItem?.marginTop ?: 0
 
                     val adapter = binding.tasksRv.adapter as PrivateTasksAdapter
                     adapter.updateData(tasks)
 
-                    layoutManager.scrollToPositionWithOffset(pos, offset - marginTop)
+//                    layoutManager.scrollToPositionWithOffset(pos, offset - marginTop)
 
                     if (tasks.size != 1) setUpNoTaskIconAndText(tasks.isNotEmpty())
                 }
--- app/src/main/java/com/rmblack/todoapp/fragments/SharedTasksFragment.kt
+++ app/src/main/java/com/rmblack/todoapp/fragments/SharedTasksFragment.kt
@@ -10,14 +10,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
-import androidx.core.view.marginTop
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.rmblack.todoapp.R
 import com.rmblack.todoapp.activities.StarterActivity
 import com.rmblack.todoapp.adapters.SharedTasksAdapter
@@ -173,16 +171,17 @@ class SharedTasksFragment : TasksFragment() {
                     editedTaskId = null
                     isNewTask = null
 
-                    val layoutManager = binding.tasksRv.layoutManager as LinearLayoutManager
-                    val firstVisibleItem = layoutManager.getChildAt(0)
-                    val offset = firstVisibleItem?.top ?: 0
-                    val pos = layoutManager.findFirstVisibleItemPosition()
-                    val marginTop = firstVisibleItem?.marginTop ?: 0
+                    // TODO: if there was no problem , delete commented code
+//                    val layoutManager = binding.tasksRv.layoutManager as LinearLayoutManager
+//                    val firstVisibleItem = layoutManager.getChildAt(0)
+//                    val offset = firstVisibleItem?.top ?: 0
+//                    val pos = layoutManager.findFirstVisibleItemPosition()
+//                    val marginTop = firstVisibleItem?.marginTop ?: 0
 
                     val adapter = binding.tasksRv.adapter as SharedTasksAdapter
                     adapter.updateData(tasks)
 
-                    layoutManager.scrollToPositionWithOffset(pos, offset - marginTop)
+//                    layoutManager.scrollToPositionWithOffset(pos, offset - marginTop)
 
                     if (tasks.size != 1) setUpNoTaskIconAndText(tasks.isNotEmpty())
                 }
