1066996276031d8517887c0ab8dbe25719f3e904
rozhiak <fhvs1234t>
2023-12-19 23:55:22 +0330

    Clean code - Implement update tasks method in viewModels.

--- app/src/main/java/com/rmblack/todoapp/viewmodels/PrivateTasksViewModel.kt
+++ app/src/main/java/com/rmblack/todoapp/viewmodels/PrivateTasksViewModel.kt
@@ -10,22 +10,26 @@ class PrivateTasksViewModel(application: Application) :
     init {
         viewModelScope.launch {
             taskRepository.getPrivateTasksFlow().collect { tasks ->
-                val sortedTasks = tasks.sortedBy { it.deadLine }
-                val tasksWithDatePositionNull = mutableListOf<Task?>()
-
-                if (sortedTasks.isNotEmpty()) {
-                    tasksWithDatePositionNull.add(null)
-                    tasksWithDatePositionNull.add(sortedTasks[0]) //Because of if condition we can not access i - 1 position (-1)
-                    for (i in 1 until sortedTasks.size) {
-                        if (sortedTasks[i].deadLine.shortDateString != sortedTasks[i - 1].deadLine.shortDateString) {
-                            tasksWithDatePositionNull.add(null)
-                        }
-                        tasksWithDatePositionNull.add(sortedTasks[i])
-                    }
-                }
-
-                _tasks.value = tasksWithDatePositionNull.toList()
+                updateTasks(tasks)
             }
         }
+    }
+
+    private fun updateTasks(tasks: List<Task>) {
+        val sortedTasks = tasks.sortedBy { it.deadLine }
+        val tasksWithDatePositionNull = mutableListOf<Task?>()
+
+        if (sortedTasks.isNotEmpty()) {
+            tasksWithDatePositionNull.add(null)
+            tasksWithDatePositionNull.add(sortedTasks[0]) //Because of if condition we can not access i - 1 position (-1)
+            for (i in 1 until sortedTasks.size) {
+                if (sortedTasks[i].deadLine.shortDateString != sortedTasks[i - 1].deadLine.shortDateString) {
+                    tasksWithDatePositionNull.add(null)
+                }
+                tasksWithDatePositionNull.add(sortedTasks[i])
+            }
+        }
+
+        _tasks.value = tasksWithDatePositionNull.toList()
     }
 }
