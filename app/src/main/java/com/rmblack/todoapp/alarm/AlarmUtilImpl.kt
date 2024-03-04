18fb90f9932e396c2b2f8e7ad3153554232bc994
rozhiak <fhvs1234t>
2024-03-04 21:01:46 +0330

    Set constraints for work manager

--- app/src/main/java/com/rmblack/todoapp/alarm/AlarmUtilImpl.kt
+++ app/src/main/java/com/rmblack/todoapp/alarm/AlarmUtilImpl.kt
@@ -1,5 +1,7 @@
 import android.content.Context
+import androidx.work.Constraints
 import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
@@ -12,11 +14,19 @@ class AlarmUtilImpl(private val context: Context) : AlarmUtil {
 
     override fun setAlarm(alarmTime: Long, taskId: UUID): Boolean {
         return try {
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+                .setRequiresBatteryNotLow(false)
+                .setRequiresCharging(false)
+                .setRequiresDeviceIdle(false)
+                .setRequiresStorageNotLow(false)
+                .build()
             val delay = alarmTime - System.currentTimeMillis()
             val workRequest = OneTimeWorkRequestBuilder<AlarmWorker>()
                 .setInitialDelay(delay, java.util.concurrent.TimeUnit.MILLISECONDS)
                 .setInputData(workDataOf(TASK_ID to taskId.toString()))
                 .addTag(taskId.toString())
+                .setConstraints(constraints)
                 .build()
 
             WorkManager.getInstance(context).enqueueUniqueWork(
