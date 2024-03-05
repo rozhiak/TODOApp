43954c3809555d5207f188aebd479aa625461277
rozhiak <fhvs1234t>
2024-03-06 00:30:54 +0330

    Remove Channel creation in application class

--- app/src/main/java/com/rmblack/todoapp/applications/TodoApplication.kt
+++ app/src/main/java/com/rmblack/todoapp/applications/TodoApplication.kt
@@ -18,28 +18,5 @@ class TodoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         TaskRepository.initialize(this)
-//        makeNotificationChannel()
-    }
-
-    private fun makeNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val soundUri =
-                Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + applicationContext.packageName + "/" + R.raw.soft_alarm_2010)
-            val channelName = "Alarm"
-            val notificationManager =
-                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val channel = NotificationChannel(
-                Constants.NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH
-            )
-            channel.description = "Notifications for alarm events"
-            channel.lightColor = Color.GREEN
-            channel.enableLights(true)
-            val audioAttributes =
-                AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .setUsage(AudioAttributes.USAGE_NOTIFICATION).build()
-
-            channel.setSound(soundUri, audioAttributes)
-            notificationManager.createNotificationChannel(channel)
-        }
     }
 }
