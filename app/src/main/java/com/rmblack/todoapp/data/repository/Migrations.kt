794c0e4dc4b17a920a19e557213aa21be81ebf7b
rozhiak <fhvs1234t>
2024-03-08 00:12:19 +0330

    Migrate database from version 1 to 2 - Change migration code

--- app/src/main/java/com/rmblack/todoapp/data/repository/Migrations.kt
+++ app/src/main/java/com/rmblack/todoapp/data/repository/Migrations.kt
@@ -3,11 +3,10 @@ package com.rmblack.todoapp.data.repository
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-
 object Migrations {
     val MIGRATION_1_2: Migration = object : Migration(1, 2) {
         override fun migrate(database: SupportSQLiteDatabase) {
-            database.execSQL("ALTER TABLE Task ADD COLUMN alarm BOOLEAN NOT NULL DEFAULT false")
+            database.execSQL("ALTER TABLE Task ADD COLUMN alarm INTEGER NOT NULL DEFAULT 0")
         }
     }
 }
