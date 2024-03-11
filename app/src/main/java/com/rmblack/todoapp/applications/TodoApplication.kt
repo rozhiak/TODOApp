be34a566cb44a763ca084d1605f1cb1d912694a5
rozhiak <fhvs1234t>
2024-03-11 14:47:57 +0330

    import deletion

--- app/src/main/java/com/rmblack/todoapp/applications/TodoApplication.kt
+++ app/src/main/java/com/rmblack/todoapp/applications/TodoApplication.kt
@@ -1,15 +1,6 @@
 package com.rmblack.todoapp.applications
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.ContentResolver
-import android.content.Context
-import android.graphics.Color
-import android.media.AudioAttributes
-import android.net.Uri
-import android.os.Build
-import com.rmblack.todoapp.R
 import com.rmblack.todoapp.data.repository.TaskRepository
 import com.rmblack.todoapp.utils.Constants
 
