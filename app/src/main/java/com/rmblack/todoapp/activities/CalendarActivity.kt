f34a23c945811dcefd01ff82e2b8718cc5d5e7a4
rozhiak <fhvs1234t>
2024-03-27 02:11:53 +0330

    Debug -> calView not showing events highlight.

--- app/src/main/java/com/rmblack/todoapp/activities/CalendarActivity.kt
+++ app/src/main/java/com/rmblack/todoapp/activities/CalendarActivity.kt
@@ -59,6 +59,7 @@ class CalendarActivity : AppCompatActivity() {
                                 false
                             )
                         )
+                        binding.calendarView.update()
                     }
                 }
             }
